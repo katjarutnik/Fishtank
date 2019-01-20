f22b66057d9de87fcb98511cf124e2ab58953a15
FishWhisperer <katja.rutnik1@student.um.si>
2019-01-20 01:53:51 +0100

    feat: intro video

---
+++ app/src/main/java/com/xd/akvarij/SplashScreenActivity.java
@@ -0,0 +1,47 @@
+package com.xd.akvarij;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try {
+            VideoView videoHolder = new VideoView(this);
+            setContentView(videoHolder);
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.fishtank_intro_new_new);
+            videoHolder.setVideoURI(video);
+
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+            });
+            videoHolder.start();
+        } catch (Exception ex) {
+            jump();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        jump();
+        return true;
+    }
+
+    private void jump() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
