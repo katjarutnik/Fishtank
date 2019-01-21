1347e72d6bfce1238d6733f9503291549f5a9986
FishWhisperer <katja.rutnik1@student.um.si>
2019-01-21 23:10:27 +0100

    fix: gravity

--- app/src/main/java/com/xd/akvarij/GameActivity.java
+++ app/src/main/java/com/xd/akvarij/GameActivity.java
@@ -37,6 +37,8 @@ public class GameActivity extends Activity implements SensorEventListener {
 
     private SensorManager sensorMan;
     private Sensor accelerometer;
+    private float[] gravity;
+    private float[] linear_acceleration;
 
     public static final String myPrefs = "myTankPrefs";
     public static final String myPrefsKey = "tank";
@@ -63,6 +65,8 @@ public class GameActivity extends Activity implements SensorEventListener {
 
         sensorMan = (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer = sensorMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gravity = new float[3];
+        linear_acceleration = new float[3];
 
         final OrientationEventListener orientationEventListener = new OrientationEventListener(this) {
             @Override
@@ -164,8 +168,14 @@ public class GameActivity extends Activity implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            if (event.values[0] > Constants.MIN_GAIN_X_LOWER ||
-                    event.values[0] < Constants.MAX_GAIN_X_LOWER) {
+            final float alpha = 0.8f;
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+            linear_acceleration[0] = event.values[0] - gravity[0];
+            linear_acceleration[1] = event.values[1] - gravity[1];
+            linear_acceleration[2] = event.values[2] - gravity[2];
+            if (linear_acceleration[0] > 0.01 || linear_acceleration[0] < -0.01) {
                 if (orientacija == 90)
                     gameView.tank.shakingStart(-event.values[1], -event.values[0]);
                 else
